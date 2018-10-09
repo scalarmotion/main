79627e47f2853ef3fa05eba5258f389dc9a6855a
Marvin Tan <37933324+marvintxd@users.noreply.github.com>
2018-10-09 23:50:37 +0800

    Add Template and TemplateSection (#65)
    * Add Template and ResumeSection classes
    
    * Rename ResumeSection to TemplateSection
    
    * Fix checkstyle issues
    
    * Add getters for TemplateSection and make it store a Category and Predicate rather than strings
    
    * Add static method to return default template
    
    * Fix checkstyle

--- src/main/java/seedu/address/model/template/Template.java
+++ src/main/java/seedu/address/model/template/Template.java
@@ -1,6 +1,9 @@
 package seedu.address.model.template;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import seedu.address.model.tag.Tag;
 
 /**
  * A template which specifies the categories and layout for the resume,
@@ -14,8 +17,23 @@ public class Template {
         this.sections = new ArrayList<TemplateSection>();
     }
 
+    /**
+     * Returns a default template with no filters
+     */
+    private static Template getDefaultTemplate() {
+        Template t = new Template();
+        t.addSection("Work Experience", "~work", new String[]{});
+        t.addSection("Education", "~education", new String[]{});
+        t.addSection("Projects", "~projects", new String[]{});
+        return t;
+    }
+
+    /**
+     * Adds a section to the Template
+     */
     public void addSection(String title, String category, String[] tags) {
-        this.sections.add(new TemplateSection(title, category, tags));
+        Predicate<Tag> predicate = t -> true;
+        this.sections.add(new TemplateSection(title, category, predicate));
     }
 
     public ArrayList<TemplateSection> getSections() {
--- src/main/java/seedu/address/model/template/TemplateSection.java
+++ src/main/java/seedu/address/model/template/TemplateSection.java
@@ -1,16 +1,33 @@
 package seedu.address.model.template;
 
+import java.util.function.Predicate;
+
+import seedu.address.model.category.Category;
+import seedu.address.model.tag.Tag;
+
 /**
  * A section of the template, representing a category with tags.
  */
 public class TemplateSection {
     private String title;
-    private String category;
-    private String[] tags;
+    private Category category;
+    private Predicate<Tag> predicate;
 
-    public TemplateSection(String title, String category, String[] tags) {
+    public TemplateSection(String title, String cateName, Predicate<Tag> predicate) {
         this.title = title;
-        this.category = category;
-        this.tags = tags;
+        this.category = new Category(cateName);
+        this.predicate = predicate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public Predicate<Tag> getPredicate() {
+        return this.predicate;
     }
 }
