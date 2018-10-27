4f5e13febbd0f285789d8ae96c728942dc4a7bee
Metta Ong <ongspxm@gmail.com>
2018-10-27 15:46:56 +0800

    create the tag command sub parser #173 (#174)
    * add tagCommand class for tag related commands to be branched from
    
    * added info to be printed in parser
    
    * framework for tag cmd parser
    
    * add test for tag cmd parser
    
    * fix checkstyle

---
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.TagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Sub parser for tag related functions
+ */
+public class TagCommandParser implements Parser<TagCommand> {
+
+    /**
+     * Used for initial separation of command word and args.
+     */
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+    private static final String MESSAGE_USAGE = TagCommand.MESSAGE_USAGE; // TODO: add usage of specific cmds
+
+    /**
+     * Parses user input into tag related commands.
+     *
+     * Parses the given {@code String} of arguments in the context of the relevant TagCommand
+     * and returns an TagCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public TagCommand parse(String userInput) throws ParseException {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+        switch (commandWord) {
+        // case AddCommand.COMMAND_WORD:
+        //     return new AddCommandParser().parse(arguments);
+
+        default:
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        }
+    }
+
+}
