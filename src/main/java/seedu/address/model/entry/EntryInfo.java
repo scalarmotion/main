17bbaac0fae762169689ff6c4d7711fd737751dc
jhengy <35480886+jhengy@users.noreply.github.com>
2018-11-01 19:35:33 +0800

    Modify Regex to enable brackets (#229)

--- src/main/java/seedu/address/model/entry/EntryInfo.java
+++ src/main/java/seedu/address/model/entry/EntryInfo.java
@@ -11,9 +11,9 @@ import java.util.List;
  */
 public class EntryInfo {
 
-    public static final String ENTRYINFO_VALIDATION_REGEX = "^[A-Za-z0-9 -]+$";
+    public static final String ENTRYINFO_VALIDATION_REGEX = "^[A-Za-z0-9 ()-]+$";
     public static final String MESSAGE_ENTRYINFO_CONSTRAINTS =
-            "header, subHeader and duration must be alphanumeric and space or dash separated";
+            "header, subHeader and duration must be alphanumeric and separated by brackets or hyphen or space.";
 
     private List<String> entryInfo = new LinkedList<String>();
 
