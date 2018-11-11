cf0265cd87753979557e22add2a0ebc6fc02a6f6
scalarmotion <harryallenxavier@gmail.com>
2018-11-11 16:38:44 +0800

    Clean imports

--- src/test/java/seedu/address/logic/commands/MakeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MakeCommandTest.java
@@ -14,7 +14,6 @@ import org.junit.rules.ExpectedException;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.template.Template;
 import seedu.address.testutil.TypicalResumeModel;
 
 public class MakeCommandTest {
